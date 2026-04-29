782026bb08d51715d29482c6a499a3fc2685e50a
Laura <laura.escobarca@campusucc.edu.co>
2026-04-28 23:59:25 -0500

    themes

---
+++ config.py
@@ -0,0 +1,16 @@
+THEMES = {
+    "light": {
+        "bg": "#FBF4EB",
+        "clock_bg": "#FBD9E5",
+        "fg": "#C43670",
+        "accent": "#F283AF",
+        "second": "#C43670"
+    },
+    "dark": {
+        "bg": "#0d0d0d",
+        "clock_bg": "#870339",
+        "fg": "#fbd9e5",
+        "accent": "#d95f8c",
+        "second": "#F283AF"
+    }
+}
