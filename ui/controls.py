120d92938169ceff4c0e6c6c2b6b628f3fb29900
Laura <laura.escobarca@campusucc.edu.co>
2026-04-29 01:05:27 -0500

    controls

--- ui/controls.py
+++ ui/controls.py
@@ -1,23 +1,29 @@
 import customtkinter as ctk
 from config import TIMEZONES
 
+
 class Controls:
     def __init__(self, parent, time_manager, toggle_theme):
 
         self.time_manager = time_manager
         self.toggle_theme = toggle_theme
-
         self.selected_zone = "Colombia"
 
-   
+        self.time_label = ctk.CTkLabel(
+            parent,
+            text="",
+            font=("Georgia", 18, "bold")
+        )
+        self.time_label.pack(pady=5)
+
         self.bar = ctk.CTkFrame(parent, corner_radius=25)
         self.bar.pack(padx=20, pady=20, fill="x")
 
         self.clock_btn = ctk.CTkButton(
             self.bar,
-            text="🕒\nReloj",
-            fg_color="transparent",
-            hover=False
+            text="🕒\nHora",
+            command=self.show_time,
+            corner_radius=20
         )
         self.clock_btn.pack(side="left", expand=True, pady=10)
 
@@ -37,10 +43,6 @@ class Controls:
         )
         self.theme_btn.pack(side="left", expand=True)
 
-        
-        self.mode_label = ctk.CTkLabel(parent, text="")
-        self.mode_label.pack(pady=5)
-
     def change_zone(self):
         zones = list(TIMEZONES.keys())
         index = zones.index(self.selected_zone)
@@ -51,25 +53,28 @@ class Controls:
 
         self.zone_btn.configure(text=f"🌍\n{self.selected_zone}")
 
+    def show_time(self):
+        h, m, s = self.time_manager.get_time()
+        self.time_label.configure(text=f"{h:02d}:{m:02d}:{s:02d}")
+
     def update_theme(self, theme, mode_name):
 
-        self.bar.configure(fg_color=theme["card"])
-
-        self.theme_btn.configure(
-            fg_color=theme["accent"],
-            text_color="white"
+        self.bar.configure(
+            fg_color=theme["card"]
         )
 
-        self.zone_btn.configure(
-            fg_color=theme["card"],
-            text_color=theme["text"]
-        )
+        for btn in [self.clock_btn, self.zone_btn, self.theme_btn]:
+            btn.configure(
+                fg_color=theme["accent"],
+                hover_color=theme["accent"],
+                text_color="white"
+            )
 
         self.clock_btn.configure(
-            text_color=theme["text"]
+            fg_color=theme["text"]
         )
 
         if mode_name == "light":
-            self.mode_label.configure(text="☀️ Modo claro", text_color=theme["text"])
+            self.time_label.configure(text_color=theme["text"])
         else:
-            self.mode_label.configure(text="🌙 Modo oscuro", text_color=theme["text"])
+            self.time_label.configure(text_color=theme["text"])
